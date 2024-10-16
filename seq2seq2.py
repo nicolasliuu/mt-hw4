d9decfa8d689e44c61abe7e491aca9c33b0ae38b
alanli79 <ali79@jh.edu>
2024-10-16 02:38:03 -0400

    new stuff

---
+++ seq2seq2.py
@@ -0,0 +1,360 @@
+import argparse
+import logging
+import random
+import time
+from io import open
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from nltk.translate.bleu_score import corpus_bleu
+from torch import optim
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+device = torch.device("cpu")
+
+SOS_token = "<SOS>"
+EOS_token = "<EOS>"
+
+SOS_index = 0
+EOS_index = 1
+MAX_LENGTH = 15
+
+
+class Vocab:
+    def __init__(self, lang_code):
+        self.lang_code = lang_code
+        self.word2index = {}
+        self.index2word = {SOS_index: SOS_token, EOS_index: EOS_token}
+        self.n_words = 2  # Count SOS and EOS
+
+    def add_sentence(self, sentence):
+        for word in sentence.split(' '):
+            self._add_word(word)
+
+    def _add_word(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+
+
+def split_lines(input_file):
+    logging.info("Reading lines of %s...", input_file)
+    lines = open(input_file, encoding='utf-8').read().strip().split('\n')
+    pairs = [l.split('|||') for l in lines]
+    return pairs
+
+
+def make_vocabs(src_lang_code, tgt_lang_code, train_file):
+    src_vocab = Vocab(src_lang_code)
+    tgt_vocab = Vocab(tgt_lang_code)
+    train_pairs = split_lines(train_file)
+
+    for pair in train_pairs:
+        src_vocab.add_sentence(pair[0])
+        tgt_vocab.add_sentence(pair[1])
+
+    logging.info('%s (src) vocab size: %s', src_vocab.lang_code, src_vocab.n_words)
+    logging.info('%s (tgt) vocab size: %s', tgt_vocab.lang_code, tgt_vocab.n_words)
+
+    return src_vocab, tgt_vocab
+
+
+def tensor_from_sentence(vocab, sentence):
+    indexes = [vocab.word2index.get(word, EOS_index) for word in sentence.split()]
+    indexes.append(EOS_index)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+
+def tensors_from_pair(src_vocab, tgt_vocab, pair):
+    input_tensor = tensor_from_sentence(src_vocab, pair[0])
+    target_tensor = tensor_from_sentence(tgt_vocab, pair[1])
+    return input_tensor, target_tensor
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(input_size, hidden_size)
+
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output, hidden = self.gru(embedded, hidden)
+        return output, hidden
+
+    def init_hidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=MAX_LENGTH):
+    encoder_hidden = encoder.init_hidden()
+
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_length = input_tensor.size(0)
+    target_length = target_tensor.size(0)
+
+    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+    loss = 0
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0, 0]
+
+    decoder_input = torch.tensor([[SOS_index]], device=device)
+    decoder_hidden = encoder_hidden
+
+    for di in range(target_length):
+        decoder_output, decoder_hidden, _ = decoder(
+            decoder_input, decoder_hidden, encoder_outputs)
+
+        loss += criterion(decoder_output, target_tensor[di])
+        decoder_input = target_tensor[di]  # Teacher forcing
+
+    loss.backward()
+
+    torch.nn.utils.clip_grad_norm_(encoder.parameters(), 5)
+    torch.nn.utils.clip_grad_norm_(decoder.parameters(), 5)
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / target_length
+
+
+def translate(encoder, decoder, sentence, src_vocab, tgt_vocab, max_length=MAX_LENGTH):
+    encoder.eval()
+    decoder.eval()
+
+    input_tensor = tensor_from_sentence(src_vocab, sentence)
+    input_length = input_tensor.size(0)
+    encoder_hidden = encoder.init_hidden()
+
+    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0, 0]
+
+    decoder_input = torch.tensor([[SOS_index]], device=device)
+    decoder_hidden = encoder_hidden
+
+    decoded_words = []
+    for di in range(max_length):
+        decoder_output, decoder_hidden, _ = decoder(
+            decoder_input, decoder_hidden, encoder_outputs)
+        topv, topi = decoder_output.data.topk(1)
+        if topi.item() == EOS_index:
+            decoded_words.append(EOS_token)
+            break
+        else:
+            decoded_words.append(tgt_vocab.index2word[topi.item()])
+
+        decoder_input = topi.squeeze().detach()
+
+    return decoded_words
+
+
+def translate_sentences(encoder, decoder, pairs, src_vocab, tgt_vocab, max_num_sentences=None, max_length=MAX_LENGTH):
+    output_sentences = []
+    for pair in pairs[:max_num_sentences]:
+        output_words = translate(encoder, decoder, pair[0], src_vocab, tgt_vocab)
+        output_sentence = ' '.join(output_words)
+        output_sentences.append(output_sentence)
+    return output_sentences
+
+
+def translate_random_sentence(encoder, decoder, pairs, src_vocab, tgt_vocab, n=1):
+    for i in range(n):
+        pair = random.choice(pairs)
+        print('>', pair[0])
+        print('=', pair[1])
+        output_words, attentions = translate(encoder, decoder, pair[0], src_vocab, tgt_vocab)
+        output_sentence = ' '.join(output_words)
+        print('<', output_sentence)
+        print('')
+
+
+def show_attention(input_sentence, output_words, attentions):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    attentions = attentions.squeeze(1).cpu().numpy()
+    cax = ax.matshow(attentions, cmap='bone')
+
+    fig.colorbar(cax)
+
+    ax.set_xticklabels([''] + input_sentence.split(' ') + [EOS_token], rotation=90)
+    ax.set_yticklabels([''] + output_words)
+
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.savefig('attention_plot.png')
+    plt.close()
+
+
+def translate_and_show_attention(input_sentence, encoder1, decoder1, src_vocab, tgt_vocab):
+    output_words, attentions = translate(
+        encoder1, decoder1, input_sentence, src_vocab, tgt_vocab)
+    print('input =', input_sentence)
+    print('output =', ' '.join(output_words))
+    show_attention(input_sentence, output_words, attentions)
+
+
+def clean(strx):
+    return ' '.join(strx.replace('@@ ', '').replace(EOS_token, '').strip().split())
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--hidden_size', default=256, type=int,
+                    help='hidden size of encoder/decoder, also word vector size')
+    ap.add_argument('--n_iters', default=10000, type=int,
+                    help='total number of examples to train on')
+    ap.add_argument('--print_every', default=5000, type=int,
+                    help='print loss info every this many training examples')
+    ap.add_argument('--checkpoint_every', default=10000, type=int,
+                    help='write out checkpoint every this many training examples')
+    ap.add_argument('--initial_learning_rate', default=0.001, type=int,
+                    help='initial learning rate')
+    ap.add_argument('--src_lang', default='fr',
+                    help='Source (input) language code, e.g. "fr"')
+    ap.add_argument('--tgt_lang', default='en',
+                    help='Source (input) language code, e.g. "en"')
+    ap.add_argument('--train_file', default='data/fren.train.bpe',
+                    help='training file. each line should have a source sentence,' +
+                         'followed by "|||", followed by a target sentence')
+    ap.add_argument('--dev_file', default='data/fren.dev.bpe',
+                    help='dev file. each line should have a source sentence,' +
+                         'followed by "|||", followed by a target sentence')
+    ap.add_argument('--test_file', default='data/fren.test.bpe',
+                    help='test file. each line should have a source sentence,' +
+                         'followed by "|||", followed by a target sentence' +
+                         ' (for test, target is ignored)')
+    ap.add_argument('--out_file', default='out.txt',
+                    help='output file for test translations')
+    ap.add_argument('--load_checkpoint', nargs=1,
+                    help='checkpoint file to start from')
+
+    args = ap.parse_args()
+
+    # Create vocab from training data or load if checkpointed
+    if args.load_checkpoint is not None:
+        state = torch.load(args.load_checkpoint[0])
+        iter_num = state['iter_num']
+        src_vocab = state['src_vocab']
+        tgt_vocab = state['tgt_vocab']
+    else:
+        iter_num = 0
+        src_vocab, tgt_vocab = make_vocabs(args.src_lang, args.tgt_lang, args.train_file)
+
+    encoder = EncoderRNN(src_vocab.n_words, args.hidden_size).to(device)
+    decoder = AttnDecoderRNN(args.hidden_size, tgt_vocab.n_words, dropout_p=0.1).to(device)
+
+    # Set up separate optimizers for encoder and decoder
+    encoder_optimizer = optim.Adam(encoder.parameters(), lr=args.initial_learning_rate)
+    decoder_optimizer = optim.Adam(decoder.parameters(), lr=args.initial_learning_rate)
+    criterion = nn.NLLLoss()
+
+    # If checkpointed, load saved weights and optimizer state
+    if args.load_checkpoint is not None:
+        encoder.load_state_dict(state['enc_state'])
+        decoder.load_state_dict(state['dec_state'])
+        encoder_optimizer.load_state_dict(state['opt_state'])
+
+    # Read training, dev, and test files
+    train_pairs = split_lines(args.train_file)
+    dev_pairs = split_lines(args.dev_file)
+    test_pairs = split_lines(args.test_file)
+
+    start = time.time()
+    print_loss_total = 0  # Reset every args.print_every
+
+    while iter_num < args.n_iters:
+        iter_num += 1
+        training_pair = tensors_from_pair(src_vocab, tgt_vocab, random.choice(train_pairs))
+        input_tensor = training_pair[0]
+        target_tensor = training_pair[1]
+
+        # Pass separate optimizers and criterion
+        loss = train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=MAX_LENGTH)
+        print_loss_total += loss
+
+        print(f'Iteration: {iter_num}, Loss: {loss:.4f}')
+
+        if iter_num % args.checkpoint_every == 0:
+            state = {'iter_num': iter_num,
+                     'enc_state': encoder.state_dict(),
+                     'dec_state': decoder.state_dict(),
+                     'opt_state': encoder_optimizer.state_dict(),
+                     'src_vocab': src_vocab,
+                     'tgt_vocab': tgt_vocab}
+            filename = 'state_%010d.pt' % iter_num
+            torch.save(state, filename)
+            logging.debug('wrote checkpoint to %s', filename)
+
+        if iter_num % args.print_every == 0:
+            print_loss_avg = print_loss_total / args.print_every
+            print_loss_total = 0
+            logging.info('time since start:%s (iter:%d iter/n_iters:%d%%) loss_avg:%.4f',
+                         time.time() - start,
+                         iter_num,
+                         iter_num / args.n_iters * 100,
+                         print_loss_avg)
+            # Translate random dev sentences for quality check
+            translate_random_sentence(encoder, decoder, dev_pairs, src_vocab, tgt_vocab, n=2)
+
+    # Translate test set and write results to the output file
+    translated_sentences = translate_sentences(encoder, decoder, test_pairs, src_vocab, tgt_vocab)
+    with open(args.out_file, 'wt', encoding='utf-8') as outf:
+        for sent in translated_sentences:
+            outf.write(clean(sent) + '\n')
+
+    # Example attention visualization
+    translate_and_show_attention("on p@@ eu@@ t me faire confiance .", encoder, decoder, src_vocab, tgt_vocab)
+
+
+if __name__ == '__main__':
+    main()
